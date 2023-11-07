6efaf7b7602d091b431c93580f754ba0dcdfb071
honglc <hongmainmail@gmail.com>
2023-11-07 17:18:50 +0700

    check iphone11 screen size

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Grid, Hidden } from "@mui/material";
 
 import Sidebar from "../Sidebar/Sidebar";
 import Topbar from "../Topbar/Topbar";
 
 function Wrapper({ children }) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+    setWindowHeight(window.innerHeight);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <Grid container>
       {/*component ẩn khi màn hình bằng và trên 600px */}
       <Hidden smUp>
         <span>mobile</span>
+        <div>
+          <p>Chiều ngang màn hình: {windowWidth}px</p>
+          <p>Chiều dọc màn hình: {windowHeight}px</p>
+        </div>
       </Hidden>
       {/*component ẩn khi màn hình nhỏ hơn (không bao gồm) 600px */}
       <Hidden smDown>
